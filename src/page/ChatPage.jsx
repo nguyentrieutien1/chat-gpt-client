f322e54923a3c0c1bee3b8ac3d75caa1a156bd50
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2024-07-05 08:55:30 +0700

    update the code

--- src/page/ChatPage.jsx
+++ src/page/ChatPage.jsx
@@ -1,11 +1,10 @@
-import { useState, useRef } from "react";
-import { IoDiamondOutline, IoSend } from "react-icons/io5";
+import React, { useState, useRef, useEffect } from "react";
+import { IoSend } from "react-icons/io5";
 import ChatMessage from "../components/message/ChatMessage";
 import UserMessage from "../components/message/UserMessage";
-import Prompt from "../components/message/Prompt";
-import LoadingMessage from "../components/loading/LoadingMessage";
 import NewChat from "../components/message/NewChat";
 import axios from "axios";
+import localStorageHelper from "../helpers/localStorageHelper";
 
 const ChatPage = () => {
   const [countChar, setCountChar] = useState(0);
@@ -18,16 +17,58 @@ const ChatPage = () => {
   };
   const [option, setOption] = useState("advance");
   const [messages, setMessages] = useState([1]);
+  const chatContainerRef = useRef(null);
+  const [conversions, setConversions] = useState([]);
 
-  const handleSubmit = (e) => {
+  useEffect(() => {
+    const conversionsFromLocalStorage =
+      localStorageHelper.localStorageHelper.getItem("conversions");
+    setConversions([...conversionsFromLocalStorage]);
+  }, [conversions.length]);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const result = axios.post("http://namnguyenproduct.com/api-convertion")
-  };
+    setValue("");
+    const result = await axios.post(
+      "https://namnguyenproduct.com/api/conversion",
+      { data: value }
+    );
 
+    if (result.status !== 201) return;
+
+    const { metadata } = result?.data;
+
+    const conversion = {
+      question: value,
+      answer: metadata,
+    };
+
+    conversions.push(conversion);
+    setConversions([...conversions]);
+    window.scrollTo({ behavior: 'smooth', top:chatContainerRef.current?.offsetTop })
+    localStorageHelper.localStorageHelper.setItem("conversions", [
+      ...conversions,
+    ]);
+    
+  };
   return (
     <div id="form_chat" className="relative h-full w-[90%] mx-auto">
-      <div className=" max-h-[680px] xl:max-h-[690px] overflow-y-scroll no-scrollbar">
-        {messages > 0 ? (
+      <div
+        className=" max-h-[580px] xl:max-h-[590px]" style={{overflowY: "scroll"}}
+      >
+        {conversions.length > 0 ? (
+          conversions.map((conversion, index) => (
+            <React.Fragment key={index}>
+              <UserMessage message={conversion?.question} />
+              <ChatMessage message={conversion?.answer} />
+            </React.Fragment>
+          ))
+        ) : (
+          <>
+            <NewChat option={option} setOption={setOption} />
+          </>
+        )}
+        {/* {messages > 0 ? (
           <>
             <UserMessage
               message={"fhsdfjksfhsjkfhsfjksjshdfksdfhskfjshdjkfhsdfsdf"}
@@ -38,21 +79,16 @@ const ChatPage = () => {
           <>
             <NewChat option={option} setOption={setOption} />
           </>
-        )}
+        )} */}
+
+        <div className="chat-container" ref={chatContainerRef} ></div>
       </div>
 
       <div className="absolute bottom-0 left-0 right-0">
-      <input
-          max={500}
-          className="m-0 bg-[#222838] text-white font-medium py-3 pl-4 pr-28 rounded-lg w-[30%] border-[0.5px] border-[#222838] hover:border-gray-200 outline-1 outline-[#222838]"
-          value={valueLink}
-          onChange={(e) => {
-            setValueLink(e.target.value);
-          }}
-          placeholder={"Link..."}
-          type="text"
-        />
-        <form onSubmit={handleSubmit} className="relative pt-3 bg-[#121521]">
+        <form
+          onSubmit={handleSubmit}
+          className="relative pt-3 bg-[#121521] fixed w-full"
+        >
           <input
             max={500}
             className="bg-[#222838] text-white font-medium py-4 pl-4 pr-28 rounded-lg w-full border-[0.5px] border-[#222838] hover:border-gray-200 outline-1 outline-[#222838] focus:outline-blue-500 transition-all"
@@ -61,18 +97,30 @@ const ChatPage = () => {
             placeholder={"Ask me anything..."}
             type="text"
           />
-          <div className="absolute top-1/2 -translate-y-1/2 right-4 flex items-center gap-2 mt-2 bg-[#139DFF] hover:bg-[#005CE6] transition-all px-3 py-1 rounded-full">
+          <div
+            onClick={handleSubmit}
+            className="hover:cursor-pointer absolute top-1/2 -translate-y-1/2 right-4 flex items-center gap-2 mt-2 bg-[#139DFF] hover:bg-[#005CE6] transition-all px-3 py-1 rounded-full"
+          >
             <IoSend size={12} className="text-white" />
             <p className="font-medium text-white text-[16px]">Send</p>
           </div>
         </form>
-        
+        {/* <input
+          max={500}
+          className="m-0 bg-[#222838] text-white font-medium py-3 pl-4 pr-28 rounded-lg w-[30%] border-[0.5px] border-[#222838] hover:border-gray-200 outline-1 outline-[#222838]"
+          value={valueLink}
+          onChange={(e) => {
+            setValueLink(e.target.value);
+          }}
+          placeholder={"Link..."}
+          type="text"
+        /> */}
 
         <p className="text-right mt-1 text-white text-lg">{countChar} /500</p>
-        <p className="text-center text-lg font-normal text-[#BBBCC1] mt-2">
+        {/* <p className="text-center text-lg font-normal text-[#BBBCC1] mt-2">
           Please be aware that this is an alpha version of the AI, and as such
           it may not always behave as expected. Use it at your own risk.
-        </p>
+        </p> */}
       </div>
     </div>
   );
