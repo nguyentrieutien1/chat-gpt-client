417cd5cd32677e29f5325428834b3429a30e5970
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2024-07-05 08:52:32 +0700

    Remove unuse the code

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import './App.css';
 import {Routes, Route} from "react-router-dom";
 import ChatPage from "./page/ChatPage";
 import ChatLayout from "./layouts/ChatLayout";
-
 function App() {
   return (
      <Routes>
-        <Route path="/" element={
+        <Route path="*" element={
             <ChatLayout>
                 <ChatPage />
             </ChatLayout>
-        } />\
+        } />
      </Routes>
   );
 }
--- src/page/ChatPage.jsx
+++ src/page/ChatPage.jsx
@@ -1,70 +1,81 @@
-import {useState, useRef} from "react";
-import {IoDiamondOutline, IoSend} from "react-icons/io5";
+import { useState, useRef } from "react";
+import { IoDiamondOutline, IoSend } from "react-icons/io5";
 import ChatMessage from "../components/message/ChatMessage";
 import UserMessage from "../components/message/UserMessage";
 import Prompt from "../components/message/Prompt";
 import LoadingMessage from "../components/loading/LoadingMessage";
 import NewChat from "../components/message/NewChat";
+import axios from "axios";
 
 const ChatPage = () => {
+  const [countChar, setCountChar] = useState(0);
+  const [value, setValue] = useState("");
+  const [valueLink, setValueLink] = useState("");
 
-    const [countChar, setCountChar] = useState(0);
-    const [value, setValue] = useState("");
-    const [valueLink, setValueLink] = useState("");
+  const handleChange = (e) => {
+    setCountChar(e.target.value.length);
+    setValue(e.target.value);
+  };
+  const [option, setOption] = useState("advance");
+  const [messages, setMessages] = useState([1]);
 
-    const handleChange = (e) => {
-        setCountChar(e.target.value.length);
-        setValue(e.target.value);
-    }
-    const [option, setOption] = useState("advance");
-    const [messages, setMessages] = useState([1]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const result = axios.post("http://namnguyenproduct.com/api-convertion")
+  };
 
+  return (
+    <div id="form_chat" className="relative h-full w-[90%] mx-auto">
+      <div className=" max-h-[680px] xl:max-h-[690px] overflow-y-scroll no-scrollbar">
+        {messages > 0 ? (
+          <>
+            <UserMessage
+              message={"fhsdfjksfhsjkfhsfjksjshdfksdfhskfjshdjkfhsdfsdf"}
+            />
+            <ChatMessage message={"sdfsdfds"} />
+          </>
+        ) : (
+          <>
+            <NewChat option={option} setOption={setOption} />
+          </>
+        )}
+      </div>
 
-    return (
-        <div id="form_chat" className="relative h-full w-[90%] mx-auto">
-            <div className=" max-h-[680px] xl:max-h-[690px] overflow-y-scroll no-scrollbar">
-                {
-                    messages > 0 ?
-                        <>
-                            <UserMessage message={"fhsdfjksfhsjkfhsfjksjshdfksdfhskfjshdjkfhsdfsdf"}/>
-                            <ChatMessage message={"sdfsdfds"}/>
-                        </>
-                        :
-                        <>
-                            <NewChat option={option} setOption={setOption}/>
-                        </>
-                }
-            </div>
+      <div className="absolute bottom-0 left-0 right-0">
+      <input
+          max={500}
+          className="m-0 bg-[#222838] text-white font-medium py-3 pl-4 pr-28 rounded-lg w-[30%] border-[0.5px] border-[#222838] hover:border-gray-200 outline-1 outline-[#222838]"
+          value={valueLink}
+          onChange={(e) => {
+            setValueLink(e.target.value);
+          }}
+          placeholder={"Link..."}
+          type="text"
+        />
+        <form onSubmit={handleSubmit} className="relative pt-3 bg-[#121521]">
+          <input
+            max={500}
+            className="bg-[#222838] text-white font-medium py-4 pl-4 pr-28 rounded-lg w-full border-[0.5px] border-[#222838] hover:border-gray-200 outline-1 outline-[#222838] focus:outline-blue-500 transition-all"
+            value={value}
+            onChange={handleChange}
+            placeholder={"Ask me anything..."}
+            type="text"
+          />
+          <div className="absolute top-1/2 -translate-y-1/2 right-4 flex items-center gap-2 mt-2 bg-[#139DFF] hover:bg-[#005CE6] transition-all px-3 py-1 rounded-full">
+            <IoSend size={12} className="text-white" />
+            <p className="font-medium text-white text-[16px]">Send</p>
+          </div>
+        </form>
+        
 
-
-            <div className="absolute bottom-0 left-0 right-0">
-                <form className="relative pt-3 bg-[#121521]">
-                    <input max={500}
-                           className="bg-[#222838] text-white font-medium py-4 pl-4 pr-28 rounded-lg w-full border-[0.5px] border-[#222838] hover:border-gray-200 outline-1 outline-[#222838] focus:outline-blue-500 transition-all"
-                           value={value}
-                           onChange={handleChange}
-                           placeholder={"Ask me anything..."}
-                           type="text"/>
-                    <div
-                        className="absolute top-1/2 -translate-y-1/2 right-4 flex items-center gap-2 mt-2 bg-[#139DFF] hover:bg-[#005CE6] transition-all px-3 py-1 rounded-full">
-                        <IoSend size={12} className="text-white"/>
-                        <p className="font-medium text-white text-[16px]">Send</p>
-                    </div>
-                    <input max={500}
-                           className="absolute -top-12 right-1 bg-[#222838] text-white font-medium py-3 pl-4 pr-28 rounded-lg w-[30%] border-[0.5px] border-[#222838] hover:border-gray-200 outline-1 outline-[#222838] focus:outline-blue-500 transition-all"
-                           value={valueLink}
-                           onChange={(e) => {setValueLink(e.target.value)}}
-                           placeholder={"Link..."}
-                           type="text"/>
-
-                </form>
-
-                <p className="text-right mt-1 text-white text-lg">{countChar} /500</p>
-                <p className="text-center text-lg font-normal text-[#BBBCC1] mt-2">Please be aware that this is an alpha
-                    version of the AI, and as such it may not always behave as expected. Use it at your own risk.</p>
-            </div>
-        </div>
-    );
-}
+        <p className="text-right mt-1 text-white text-lg">{countChar} /500</p>
+        <p className="text-center text-lg font-normal text-[#BBBCC1] mt-2">
+          Please be aware that this is an alpha version of the AI, and as such
+          it may not always behave as expected. Use it at your own risk.
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default ChatPage;
